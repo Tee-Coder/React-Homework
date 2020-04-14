97f3eba9a67c1cfbb226bf4763233c20f03d1daf
Toyin <oluwatoyinogundare@gmail.com>
2020-04-14 02:41:55 -0600

    tried navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import LOGIN from './webpage-component/LOGIN'
 import TheModalForm from './webpage-component/TheModalForm'
+import Navbar from './webpage-component/Navbar'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar name = "" />
       <LOGIN />
       <TheModalForm/>
       
